62689e9623690ef35923cd7f98c14f1899d26915
Daniel Neves <73949890+Dan7Arievlis@users.noreply.github.com>
2021-09-23 20:40:22 -0300

    Update Stack.hs

--- Stack.hs
+++ Stack.hs
@@ -1,4 +1,4 @@
-module Stack (Stack, new, isEmpty, push, pop, peek, contains, search) where
+module Stack (Stack, new, pop, push, isEmpty) where
 
 data Stack t = Stack [t] deriving Show
 
@@ -15,20 +15,3 @@ push (Stack stack) elem = Stack (elem : stack)
 pop :: Stack t -> (t, Stack t)
 pop (Stack []) = error "Pilha vazia"
 pop (Stack (a:b)) = (a, Stack b)
-
-peek :: Stack t -> t
-peek (Stack (a:b)) = a
-
-contains :: (Eq t) => Stack t -> t -> Bool
-contains (Stack []) value = False
-contains (Stack (a:b)) value
-  | a == value = True
-  | otherwise = contains (Stack b) value
-
-search :: (Eq t) => Stack t -> t -> Int
-search (Stack stack) value = search' stack value 0
-
-search' [] value index = error "Item nao encontrado"
-search' (a:b) value index
-  | value == a = index
-  | otherwise = search' b value (succ index)
